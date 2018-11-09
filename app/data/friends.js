849736e278ee9376f0dd874dbf974f6c6123cad0
Loafus <eth.fish@gmail.com>
2018-11-08 22:12:45 -0800

    all done :)

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+
+var friends = [
+    {
+        name: "Ethan",
+        photo: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/39521810_2039068972791486_5256817911904862208_n.jpg?_nc_cat=102&_nc_ht=scontent-sea1-1.xx&oh=79887b44c02104df4e2edf8215949a5f&oe=5C43C297",
+        scores: [
+            2,
+            2,
+            4,
+            1,
+            4,
+            1,
+            4,
+            1,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Shmethan",
+        photo: "https://i.kym-cdn.com/entries/icons/facebook/000/003/047/omg.jpg",
+        scores: [
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5
+        ]
+    }
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+
+var friendsData = require("../data/friends");
+
+module.exports = function (app) {
+
+  app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function (req, res) {
+    // TODO
+    // compatability logic
+    // find the total difference between each users answers and save the results in a parallel array?
+    var userScores = req.body.scores
+    var totalDiffs = [];
+
+    for (i = 0; i < friendsData.length; i++) {
+      var totalDiff = 0;
+      for (j = 0; j < userScores.length; j++) {
+        totalDiff += Math.abs(userScores[j] - friendsData[i].scores[j])
+      }
+      totalDiffs.push(totalDiff)
+    }
+
+    // finds the min difference and saves the friend as match
+    var match = friendsData[totalDiffs.indexOf(Math.min.apply(null, totalDiffs))]
+
+
+    res.json(match)
+    // push to api
+    friendsData.push(req.body);
+  });
+
+  app.post("/api/clear", function (req, res) {
+    friendsData.length = [];
+  });
+};
